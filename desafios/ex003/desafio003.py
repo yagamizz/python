2d74e72f27bc9ee9430181c0d60234d015a2cd87
yagamizz <158341089+yagamizz@users.noreply.github.com>
2024-03-01 18:37:52 -0300

    att

---
+++ desafios/ex003/desafio003.py
@@ -0,0 +1,10 @@
+# n1 = int(input('digite um número: '))
+#n2 = int(input('digite outro número: '))
+#s = n1 + n2
+#print('a soma entre {} e {} é igual a {}!' .format (n1, n2, s))
+
+
+n1 = int(input('digite um número: '))
+n2 = int(input('digite outr número: '))
+s = n1 + n2
+print(f'a soma entre {n1} e {n2} é igual a{s}!')
